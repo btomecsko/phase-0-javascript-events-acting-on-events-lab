3bfb1bbe8e11d1b6fe35780c43086f7b16d4004c
btomecsko <brandon.tomecsko92@gmail.com>
2022-05-03 17:22:26 -0500

    commit tree 2b18de9d91b9a3cc4bac8f1acdc07055c929a0ef

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+//Dodger reference
+
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = '#8a0303';
+
+//Dodger position
+
+//dodger.style.bottom = "0px";
+//dodger.style.left = '0px';
+
+//Dodger event
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+  });
+  
